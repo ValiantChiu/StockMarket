dd3e5de7bf7200543aebdca63921e63402cdf8db
ValiantChiu <lovaliantpoine@hotmail.com>
2021-12-26 16:48:26 +0800

    00631L strategy saved

--- StockMarket/00631L Strategy.R
+++ StockMarket/00631L Strategy.R
@@ -5,8 +5,10 @@ library(jsonlite)
 library(rvest)
 library(lubridate)
 library(TTR)
-stock_price <- readRDS('00631L Strategy/stock_price.rds')
-
+stock_price <- readRDS('00631L Strategy/stock_price_L.rds') %>% select(-nrown)
+stock_list <- stock_price %>% select(公司代號) %>% unique
+stock <- stock_list$`公司代號`[2]
+stock_price <- stock_price %>% filter(`公司代號` == stock)
 #KD Calculation
 GetKD <- function(stock_price) {
     GetRSV <- function(C, D, n) {
@@ -41,97 +43,118 @@ stock_price_all <- stock_price_all %>% rename(open = 開盤價, highr = 最高價, low
 
 #tunning
 k_buy_parameter <- 20
-k_sell_parameter <- 80
+k_sell_parameter <- 95
 
-GetActionHistory <- function(k_buy_parameter, k_sell_parameter) {
+GetActionHistory <- function(k_buy_parameter, k_sell_parameter,type='profit') {
+    print(k_buy_parameter)
+    print(k_sell_parameter)
     buy_point <- stock_price_all %>% filter(k < k_buy_parameter)
     sell_point <- stock_price_all %>% filter(k > k_sell_parameter)
-    GetBuySellInfo <- function(buy_time) {
-        buy_info <- stock_price_all %>% filter(day > buy_time) %>% arrange(day) %>% .[1,] %>% select(-c(公司代號)) %>% rename_all(~paste0(., '_buy'))
-        sell_info <- sell_point %>% filter(day > buy_time) %>% arrange(day) %>% .[1,] %>% select(-c(公司代號)) %>% rename_all(~paste0(., '_sell'))
-        cbind(buy_info, sell_info) %>% as.tibble
-    }
-    buy_sell_info <- buy_point %>% mutate(buysellinfo = map(day, GetBuySellInfo)) %>% unnest
-    buy_sell_info <- buy_sell_info %>% group_by(day_sell) %>% filter(day_buy == min(day_buy))
-
-
-    profit_buy_info <- buy_sell_info %>% ungroup %>% filter(open_sell >= open_buy)
-    non_profit_buy <- buy_sell_info %>% ungroup %>% filter(open_sell < open_buy) %>% select(公司代號, open, highr, low, close, day, RSV, k, d)
-
-    GetNonProfitBuySellInfo <- function(buy_time) {
-        buy_info <- stock_price_all %>% filter(day > buy_time) %>% arrange(day) %>% .[1,] %>% select(-c(公司代號)) %>% rename_all(~paste0(., '_buy'))
-        buy_price <- buy_info$open_buy[1]
-        sell_info <- sell_point %>% filter(day > buy_time) %>% arrange(day) %>% .[1,] %>% select(-c(公司代號)) %>% rename_all(~paste0(., '_sell'))
-        sell_day <- sell_info$day_sell[1]
-        sell_info <- stock_price_all %>% filter(day > sell_day & open > buy_price) %>% arrange(day) %>% .[1,] %>% select(-c(公司代號)) %>% rename_all(~paste0(., '_sell'))
-        cbind(buy_info, sell_info) %>% as.tibble
-    }
-    non_profit_buy_info <- non_profit_buy %>% mutate(buysellinfo = map(day, GetNonProfitBuySellInfo)) %>% unnest
-    buysellinfo <- rbind(profit_buy_info, non_profit_buy_info) %>% arrange(day)
-
-    FilterDay <- function(buysellinfo) {
-        filter_day <- tibble()
-        for (i in 1:nrow(buysellinfo)) {
-            buy_day <- buysellinfo$day_buy[i]
-            sell_day <- buysellinfo$day_sell[i]
-            one_filter_day <- buysellinfo %>% filter(day_buy > buy_day & day_sell < sell_day) %>% select(day_buy)
-            filter_day <- rbind(filter_day, one_filter_day)
+    if (nrow(buy_point) == 0 | nrow(sell_point) ==0) {
+        tibble()
+    } else {
+        GetBuySellInfo <- function(buy_time) {
+            buy_info <- stock_price_all %>% filter(day > buy_time) %>% arrange(day) %>% .[1,] %>% select(-c(公司代號)) %>% rename_all(~paste0(., '_buy'))
+            sell_info <- sell_point %>% filter(day > buy_time) %>% arrange(day) %>% .[1,] %>% select(-c(公司代號)) %>% rename_all(~paste0(., '_sell'))
+            cbind(buy_info, sell_info) %>% as.tibble
         }
-        filter_day
+        buy_sell_info <- buy_point %>% mutate(buysellinfo = map(day, GetBuySellInfo)) %>% unnest
+        buy_sell_info <- buy_sell_info %>% group_by(day_sell) %>% filter(day_buy == min(day_buy))
+
+
+        profit_buy_info <- buy_sell_info %>% ungroup %>% filter(open_sell >= open_buy)
+        non_profit_buy <- buy_sell_info %>% ungroup %>% filter(open_sell < open_buy) %>% select(公司代號, open, highr, low, close, day, RSV, k, d)
+
+        GetNonProfitBuySellInfo <- function(buy_time) {
+            buy_info <- stock_price_all %>% filter(day > buy_time) %>% arrange(day) %>% .[1,] %>% select(-c(公司代號)) %>% rename_all(~paste0(., '_buy'))
+            buy_price <- buy_info$open_buy[1]
+            sell_info <- sell_point %>% filter(day > buy_time) %>% arrange(day) %>% .[1,] %>% select(-c(公司代號)) %>% rename_all(~paste0(., '_sell'))
+            sell_day <- sell_info$day_sell[1]
+            sell_info <- stock_price_all %>% filter(day > sell_day & open > buy_price) %>% arrange(day) %>% .[1,] %>% select(-c(公司代號)) %>% rename_all(~paste0(., '_sell'))
+            cbind(buy_info, sell_info) %>% as.tibble
+        }
+        non_profit_buy_info <- non_profit_buy %>% mutate(buysellinfo = map(day, GetNonProfitBuySellInfo)) %>% unnest
+        buysellinfo <- rbind(profit_buy_info, non_profit_buy_info) %>% arrange(day)
+
+        FilterDay <- function(buysellinfo) {
+            filter_day <- tibble()
+            for (i in 1:nrow(buysellinfo)) {
+                buy_day <- buysellinfo$day_buy[i]
+                sell_day <- buysellinfo$day_sell[i]
+                one_filter_day <- buysellinfo %>% filter(day_buy > buy_day & day_buy < sell_day) %>% select(day_buy)
+                filter_day <- rbind(filter_day, one_filter_day)
+            }
+            filter_day
+        }
+        filter_list <- FilterDay(buysellinfo)
+        action_history <- buysellinfo %>% filter(!(day_buy %in% filter_list$day_buy))
+        action_history <- action_history %>% mutate(profit = (open_sell - open_buy) / open_buy)
+        if (type =='profit') {
+            action_history$profit %>% sum
+        } else {
+            action_history
+        }
     }
-    filter_list <- FilterDay(buysellinfo)
-    action_history <- buysellinfo %>% filter(!(day_buy %in% filter_list$day_buy))
-    action_history <- action_history %>% mutate(profit = (open_sell - open_buy) / open_buy)
-    action_history$profit %>% sum
+
+
 }
 
+k_buy <- tibble(k_buy_parameter = seq(5, 40, by = 5))
+k_sell <- tibble(k_sell_parameter = seq(60, 95, by = 5))
+k_buy_k_sell_table <- k_buy %>% mutate(sp = list(k_sell)) %>% unnest
 
-write.csv(action_history, file = '00631L Strategy/buysellinfo.csv')
+action_history_profit <- k_buy_k_sell_table %>% mutate(profit = map2(k_buy_parameter, k_sell_parameter, GetActionHistory))
+action_history_profit <- action_history_profit %>% unnest
 
+write.csv(action_history_profit, file = paste0('00631L Strategy/action_history_profit_', stock, '.csv'))
+
+write.csv(GetActionHistory(30, 95, 'history'), file = paste0('00631L Strategy/buysellinfo_', stock, '.csv'))
 #Day Trade
+if (F) {
 
-SL <- 0.2 / 100
-SP <- 0.3 / 100
-GetTotalProfit <- function(SL, SP) {
-    Tf <- (0.1425 + 0.1) / 100
-    GetProfit <- function(O, H, L, C) {
-        if (
-            ((O - L) / O + Tf) >= SL
-            ) {
-            -SL
-        }
-        else if (
-            ((H - O) / O - Tf) >= SP
-            ) {
-            SP
-        } else {
-            (C - O) / O - Tf
-        }
-    }
-    if (T) {
+    SL <- 0.2 / 100
+    SP <- 0.3 / 100
+    GetTotalProfit <- function(SL, SP) {
+        Tf <- (0.1425 + 0.1) / 100
         GetProfit <- function(O, H, L, C) {
             if (
-                ((H - O) / O - Tf) >= SP
-                ) {
-                SP
-            }
-            else if (
                 ((O - L) / O + Tf) >= SL
                 ) {
                 -SL
+            }
+            else if (
+                ((H - O) / O - Tf) >= SP
+                ) {
+                SP
             } else {
                 (C - O) / O - Tf
             }
         }
+        if (T) {
+            GetProfit <- function(O, H, L, C) {
+                if (
+                    ((H - O) / O - Tf) >= SP
+                    ) {
+                    SP
+                }
+                else if (
+                    ((O - L) / O + Tf) >= SL
+                    ) {
+                    -SL
+                } else {
+                    (C - O) / O - Tf
+                }
+            }
+        }
+
+        profit_result <- stock_price %>% mutate(profit = pmap(list(開盤價, 最高價, 最低價, 收盤價), GetProfit) %>% as.numeric)
+        profit_result$profit %>% sum
     }
 
-    profit_result <- stock_price %>% mutate(profit = pmap(list(開盤價, 最高價, 最低價, 收盤價), GetProfit) %>% as.numeric)
-    profit_result$profit %>% sum
+
+    SL_ <- tibble(SL = seq(0.003, 0.03, by = 0.001))
+    SP_ <- tibble(SP = seq(0.003, 0.03, by = 0.001))
+    SL_SP_table <- SL_ %>% mutate(sp = list(SP_)) %>% unnest
+
+    SL_SP_table %>% mutate(Total_Porift = map2(SL, SP, GetTotalProfit) %>% as.numeric) %>% arrange(desc(Total_Porift))
 }
-
-
-SL_ <- tibble(SL = seq(0.003, 0.03, by = 0.001))
-SP_ <- tibble(SP = seq(0.003, 0.03, by = 0.001))
-SL_SP_table <- SL_ %>% mutate(sp = list(SP_)) %>% unnest
-
-SL_SP_table %>% mutate(Total_Porift = map2(SL, SP, GetTotalProfit) %>% as.numeric) %>% arrange(desc(Total_Porift))
